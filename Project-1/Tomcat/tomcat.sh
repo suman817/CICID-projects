ec9953e87434fddad607985b53f6c74ba2146bd4
suman <85454271+suman817@users.noreply.github.com>
2021-07-26 08:28:16 +0530

    Update tomcat.sh

--- Project-1/Tomcat/tomcat.sh
+++ Project-1/Tomcat/tomcat.sh
@@ -5,12 +5,12 @@ cd /opt/
 echo "Download Tomcat"
 wget $Tomcat_url
 echo "Extract Tomcat file"
-tar xvf apache-tomcat-8.5.61.tar.gz
+tar xvf apache-tomcat-9.0.50.tar.gz
 echo "Change Directory name"
-mv apache-tomcat-8.5.61 tomcat-8
+mv apache-tomcat-9.0.50.tar.gz tomcat-9
 cp /root/CICID-projects/Project-1/Tomcat/context.xml /opt/tomcat-9/webapps/manager/META-INF/
 cp /root/CICID-projects/Project-1/Tomcat/context.xml /opt/tomcat-9/webapps/host-manager/META-INF/
 cp /root/CICID-projects/Project-1/Tomcat/tomcat-users.xml /opt/tomcat-9/conf/
 echo "Start Tomcat server"
-bash /opt/tomcat-8/bin/startup.sh
+bash /opt/tomcat-9/bin/startup.sh
 
